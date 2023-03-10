86ea9849c2d9d8dfa077c8661568871982f8be30
jaxoncreed <jaxoncreed@gmail.com>
2023-03-10 16:22:46 -0500

    Full test coverage

--- lib/datasetConverters.ts
+++ lib/datasetConverters.ts
@@ -1,5 +1,4 @@
 import { Dataset } from "@rdfjs/types";
-import { ContextDefinition, JsonLdDocument } from "jsonld";
 import { Writer, WriterOptions } from "n3";
 // import SerializerJsonld from "@rdfjs/serializer-jsonld";
 // import { Readable } from "readable-stream";
@@ -23,31 +22,30 @@ export async function datasetToString(
   });
 }
 
-export async function datasetToJsonLd(
-  _dataset: Dataset,
-  _context: ContextDefinition
-): Promise<JsonLdDocument> {
-  throw new Error("Not Implemented");
-  // return new Promise((resolve, reject) => {
-  //   const serializerJsonld = new SerializerJsonld();
-  //   const input = new Readable({
-  //     objectMode: true,
-  //     read: () => {
-  //       dataset.forEach((quad) => {
-  //         input.push(quad);
-  //       });
-  //       input.push(null);
-  //     },
-  //   });
-  //   const output = serializerJsonld.import(input);
+// export async function datasetToJsonLd(
+//   dataset: Dataset,
+//   context: ContextDefinition
+// ): Promise<JsonLdDocument> {
+//   return new Promise((resolve, reject) => {
+//     const serializerJsonld = new SerializerJsonld();
+//     const input = new Readable({
+//       objectMode: true,
+//       read: () => {
+//         dataset.forEach((quad) => {
+//           input.push(quad);
+//         });
+//         input.push(null);
+//       },
+//     });
+//     const output = serializerJsonld.import(input);
 
-  //   output.on("data", (jsonld) => {
-  //     resolve(jsonld);
-  //   });
-  //   /* istanbul ignore next */
-  //   output.on("error", (err) => {
-  //     /* istanbul ignore next */
-  //     reject(err);
-  //   });
-  // });
-}
+//     output.on("data", (jsonld) => {
+//       resolve(jsonld);
+//     });
+//     /* istanbul ignore next */
+//     output.on("error", (err) => {
+//       /* istanbul ignore next */
+//       reject(err);
+//     });
+//   });
+// }
