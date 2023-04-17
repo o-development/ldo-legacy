b8ecd58810459c9be6bc4d1a15da6bccb155de11
jaxoncreed <jaxoncreed@gmail.com>
2023-04-16 21:02:42 -0400

    Language support

--- example/example.ts
+++ example/example.ts
@@ -54,9 +54,24 @@ async function run() {
    * Step 3: Convert it back to RDF
    */
   // Logs:
-
+  // <https://solidweb.me/jane_doe/profile/card#me> a <http://xmlns.com/foaf/0.1/Person>;
+  //   <http://xmlns.com/foaf/0.1/name> "Jane Smith";
+  //   <http://xmlns.com/foaf/0.1/knows> <https://solidweb.me/john_smith/profile/card#me>.
+  // <https://solidweb.me/john_smith/profile/card#me> a <http://xmlns.com/foaf/0.1/Person>;
+  //   <http://xmlns.com/foaf/0.1/name> "John Smith";
+  //   <http://xmlns.com/foaf/0.1/knows> <https://solidweb.me/jane_doe/profile/card#me>.
   console.log(await toTurtle(janeProfile));
   // Logs:
+  // DELETE DATA {
+  //   <https://solidweb.me/jane_doe/profile/card#me> <http://xmlns.com/foaf/0.1/name> "Jane Doe" .
+  // };
+  // INSERT DATA {
+  //   <https://solidweb.me/jane_doe/profile/card#me> <http://xmlns.com/foaf/0.1/name> "Jane Smith" .
+  //   <https://solidweb.me/jane_doe/profile/card#me> <http://xmlns.com/foaf/0.1/knows> <https://solidweb.me/john_smith/profile/card#me> .
+  //   <https://solidweb.me/john_smith/profile/card#me> <http://xmlns.com/foaf/0.1/name> "John Smith" .
+  //   <https://solidweb.me/john_smith/profile/card#me> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Person> .
+  //   <https://solidweb.me/john_smith/profile/card#me> <http://xmlns.com/foaf/0.1/knows> <https://solidweb.me/jane_doe/profile/card#me> .
+  // }
   console.log(await toSparqlUpdate(janeProfile));
 }
 run();
